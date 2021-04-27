5bec44e596889c0170f449e60552b82447c50f5f
joaohitallo <joaohitallo97@gmail.com>
2021-04-27 01:04:28 -0300

    Estrututando formulario

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -20,18 +21,19 @@ export function App() {
   } 
   return (
     <>
-      <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Header  
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
 
       <Dashboard />
 
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
+
     </>
   );
 }

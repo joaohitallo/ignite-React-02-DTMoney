d95c36e017f9bf884d34803df703bbf74bebc759
joaohitallo <joaohitallo97@gmail.com>
2021-05-18 00:33:13 -0300

    Funcionamento dos botoes

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -57,12 +57,18 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
 
-    button {
-        height: 4rem;
+`;
+
+interface RadioBoxProps {
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
 
         display: flex;
         align-items: center;
@@ -86,5 +92,4 @@ export const TransactionTypeContainer = styled.div`
             color: var(--text-title);
 
         }
-    }
 `;

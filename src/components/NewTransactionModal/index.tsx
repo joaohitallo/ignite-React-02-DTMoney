8abdb5317ae429ed2794a86d294de3fcb15ded0b
joaohitallo <joaohitallo97@gmail.com>
2021-05-19 20:32:54 -0300

    Adicionando cores aos botoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -47,6 +47,7 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
           type="button"
           onClick={() => { setType('deposit'); }}
           isActive={type === 'deposit'}
+          activeColor="green"
         >
           <img src={incomeImg} alt="Entrada" />
           <span> Entrada </span>
@@ -57,6 +58,7 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
           type="button"
           onClick={() => { setType('withdraw'); }}
           isActive={type === 'withdraw'}
+          activeColor="red"
         >
           <img src={outcomeImg} alt="Saida" />
           <span> Sáida </span>
